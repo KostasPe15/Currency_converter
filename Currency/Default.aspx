2a4b4176e9f4143b41f8311ceb88b2f9bb9d3c6a
Konstantinos Petsanoukis <kostaspe01@gmail.com>
2022-04-28 01:33:35 +0300

    Add files via upload

---
+++ Currency/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>Μετατροπή Συναλλαγμάτος</h1>
+        Δώστε νόμισμα προέλευσης:
+        <asp:TextBox ID="TextBox1" runat="server" Width="39px"></asp:TextBox>
+&nbsp;<asp:DropDownList ID="lstCurFrom" runat="server" Height="16px" Width="234px">
+        </asp:DropDownList>
+        <br />
+        <br />
+        Δώστε νόμισμα προορισμού:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="lstCurTo" runat="server" Height="25px" Width="226px">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Μετατροπή" />
+        <br />
+        <br />
+        Αποτέλεσμα: <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Currency/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class _Default : System.Web.UI.Page
+{
+    lt.lb.www.ExchangeRates myWS2 = new lt.lb.www.ExchangeRates();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack == false)
+        {
+            XmlNode node = myWS2.getListOfCurrencies();
+
+            XmlNodeList nodes = node.SelectNodes("//item");
+
+            foreach (XmlNode nd in nodes)
+            {
+                lstCurFrom.Items.Add(nd["currency"].InnerText +
+                    " " + nd["description"].InnerText);
+                lstCurTo.Items.Add(nd["currency"].InnerText +
+                    " " + nd["description"].InnerText);
+            }
+            lstCurFrom.SelectedIndex = 26;
+            lstCurTo.SelectedIndex = 27;
+        }
+        
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        decimal f, rateFrom = 0, rateTo =0;
+        string n1, n2;
+
+        n1 = Convert.ToString(lstCurFrom.SelectedItem).Substring(0,3);
+        n2 = Convert.ToString(lstCurTo.SelectedItem).Substring(0, 3);
+
+        XmlNode node = myWS2.getExchangeRatesByDate("2014-12-31");
+        XmlNodeList nodes = node.SelectNodes("//item");
+        foreach (XmlNode nde in nodes)
+        {
+            if (nde["currency"].InnerText == n1 )
+                rateFrom = Convert.ToDecimal(nde["rate"].InnerText) / 
+                Convert.ToInt32(nde["quantity"].InnerText);
+
+            if (nde["currency"].InnerText == n2)
+                rateTo = Convert.ToDecimal(nde["rate"].InnerText) /
+                Convert.ToInt32(nde["quantity"].InnerText);
+
+        }
+        f = Math.Round(Convert.ToDecimal(TextBox1.Text) * rateFrom / rateTo, 2);
+        Label1.Text = Convert.ToString(f);
+    }
+}
